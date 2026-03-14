75a040e8d6c585b1cd47860073315b639216ee56
Ali Ahmad <ali.ahm3892@gmail.com>
2026-03-15 02:11:27 +0500

    Initial commit - MERN E-commerce with admin, frontend & backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect DB
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+app.use(cors());
+
+// Api Endpoints
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
